fb0bf3e8afedefffea9bb7b5a36861702721a843
현준 이 <msh794000@gmail.com>
2021-06-02 11:12:42 +0900

    푸시

---
+++ src/components/Content1.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="mainContent">
+    <div>
+      <h1>Business area</h1>
+      <div class="contentTop">
+        <div>
+          <div class="image1"></div>
+          <div>
+            <p>01</p>
+            <span>비즈니스 리모델링</span>
+          </div>
+        </div>
+        <div>
+          <div class="image2"></div>
+          <div>
+            <p>02</p>
+            <span>미래전략 설계</span>
+          </div>
+        </div>
+        <div>
+          <div class="image3"></div>
+          <div>
+            <p>03</p>
+            <span>컨설팅</span>
+          </div>
+        </div>
+      </div>
+      <div class="contentBottom">
+        <div>
+          <div class="image4"></div>
+          <div>
+            <p>04</p>
+            <span>AI 모델 계발</span>
+          </div>
+        </div>
+        <div>
+          <div class="image5"></div>
+          <div>
+            <p>05</p>
+            <span>AI 데이터 가공</span>
+          </div>
+        </div>
+        <div>
+          <div class="image6"></div>
+          <div>
+            <p>06</p>
+            <span>초기투자</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+  .mainContent{
+    max-width: 1920px;
+    width: 100%;
+    height: 800px;
+    background-image: url("../assets/backBuilding.png");
+
+    > div{
+      margin: 0 auto;
+      max-width: 1200px;
+      width: 100%;
+      padding-top: 87px;
+      
+      >h1{
+        margin-bottom: 50px;
+        font-family: GmarketSansTTFBold ,sans-serif;
+        color: #0d1741;
+      }
+
+      .contentTop{
+        margin-bottom: 48px;
+      }
+
+      > div{
+        width: 100%;
+        height: 235px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        >div{
+          height: 100%;
+          width: 380px;
+          // box-shadow: ;
+          box-shadow:3px 10px 20px #d8d8d8;
+
+          .image1{
+            background-image: url("../assets/1.png");
+          }
+          .image2{
+            background-image: url("../assets/2.png");
+          }
+          .image3{
+            background-image: url("../assets/3.png");
+          }
+          .image4{
+            background-image: url("../assets/4.png");
+          }
+          .image5{
+            background-image: url("../assets/5.png");
+          }
+          .image6{
+            background-image: url("../assets/6.png");
+          }
+
+          > div:first-child{
+            height: 151px;
+            // background:orange ;
+          }
+
+          > div:last-child{
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            padding-left: 27px;
+            height: 85px;
+            background: white ;
+
+            p{
+             color: #01bc9a; 
+             font-size: 26px;
+             display: flex;
+             align-items: center;
+             font-family: GmarketSansTTFBold ,sans-serif;
+            }
+
+            p::after{
+              content: "";
+              display: block;
+              height: 23px;
+              width: 1px;
+              background:#5e6480;
+              margin:0 10px;
+            }
+            
+            span{
+              color: #0d1741;
+              font-size: 26px;
+              font-family: GmarketSansTTFMedium ,sans-serif;
+            }
+          }
+        }
+      }
+    }
+  }
+</style>>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="Navs">
+    <div>
+      <img src="../assets/logo.png" alt="Logo">
+      <div>
+        <p>About</p> 
+        <p>Business</p> 
+        <p>area</p> 
+        <p>Target</p> 
+        <p>Our goal</p> 
+        <p>Works</p> 
+        <p>Partners</p> 
+        <p>Contact</p> 
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+// import Logo from "@/assets/logo.png"
+
+export default {
+  components: {
+    // Logo
+  }
+}
+</script>
+
+<style  lang="scss">
+  .Navs{
+    position: absolute;
+    top: 55px;
+    width: 100%;
+
+    >div{
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      max-width: 1200px;
+      width: 100%;
+      margin: 0 auto;
+
+
+      >div{
+        display: flex; 
+        align-items: center;
+
+        p{
+          color: white;
+          margin-left: 24px;
+          position: relative;
+          transition: .3s;
+          font-family: 'Objektiv Mk2 W03 Medium' ,sans-serif;
+          font-size: .875rem;
+        }
+
+
+
+        p:hover{
+          cursor: pointer;
+        }
+
+        p::after{
+          content: "";
+          opacity: 0;
+        }
+
+        p:hover::after{
+          content: "";
+          width: 100%;
+          opacity: 1;
+          height: 1px;
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          background:white;
+          -webkit-transition: all .3s;
+          transition: all .3s;
+        }
+      }
+    }
+  }
+</style>
